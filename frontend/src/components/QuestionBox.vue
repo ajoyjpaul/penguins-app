5667c76816e763f27a8e6175d85a859d03e261cb
ajoyroi <126199067+ajoyroi@users.noreply.github.com>
2025-06-08 02:06:03 -0400

    Change submit from magnifying glass to arrow.

--- frontend/src/components/QuestionBox.vue
+++ frontend/src/components/QuestionBox.vue
@@ -13,8 +13,8 @@
         @click="handleSubmit"
         type="button"
       >
-        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
-          <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85zm-5.442 1.05a5.5 5.5 0 1 1 0-11 5.5 5.5 0 0 1 0 11"/>
+        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-arrow-right" viewBox="0 0 16 16">
+          <path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8"/>
         </svg>
       </button>
     </div>
