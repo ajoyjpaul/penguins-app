e0cb26ed4fde846a250cceca60f4db588fe91b8e
ajoyroi <126199067+ajoyroi@users.noreply.github.com>
2025-06-08 00:31:59 -0400

    Add home page to route.

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,8 +1,10 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
 import LoginPage from "../components/LoginPage.vue";
+import HomePage from "../components/HomePage.vue";
 
 const routes: RouteRecordRaw[] = [
   { path: "/login", name: "Login", component: LoginPage },
+  { path: "/home", name: "Home", component: HomePage },
 ];
 
 const router = createRouter({
