d6dfd5abded0e1123e68eea98fdc000f51e6f558
ajoyroi <126199067+ajoyroi@users.noreply.github.com>
2025-06-08 02:00:24 -0400

    Add Penguins AI to router and css changes.

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,8 +1,10 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
 import LoginPage from "../views/LoginPage.vue";
 import HomePage from "../views/HomePage.vue";
+import PenguinsAI from "../views/PenguinsAI.vue";
 
 const routes: RouteRecordRaw[] = [
+  { path: "/AI", name: "PenguinsAI", component: PenguinsAI },
   { path: "/login", name: "Login", component: LoginPage },
   { path: "/home", name: "Home", component: HomePage },
 ];
